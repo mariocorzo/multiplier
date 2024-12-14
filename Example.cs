12d66b6644a0eb0d63cfa8b2474639f89b94ae58
mcorzo <mariodanielcorzo@gmail.com>
2024-12-14 10:34:09 -0300

    Initial commit

---
+++ Example.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace McorzoMultiplyNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Usar la función Multiply de MathHelper
+            int result = MathHelper.Multiply(5, 3);
+            Console.WriteLine($"El resultado de 5 x 3 es: {result}");
+        }
+    }
+}
---
+++ MathHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace McorzoMultiplyNumbers;
+
+public class MathHelper
+{
+    /// <summary>
+    /// Multiplica dos números y devuelve el resultado.
+    /// </summary>
+    /// <param name="a">El primer número.</param>
+    /// <param name="b">El segundo número.</param>
+    /// <returns>El producto de los dos números.</returns>
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
